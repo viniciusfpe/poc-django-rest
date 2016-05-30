c4c514e09c5a376b4d7891acfd574f40b6ea0f99
Mateus Flavio <mateusflavio@gmail.com>
2016-05-30 14:53:40 -0300

    add client mysql

--- django/poc-django-rest/settings/development.py
+++ django/poc-django-rest/settings/development.py
@@ -2,19 +2,31 @@ from .base import *
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'dev.db',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
+URL_TESTE = 'DEV'
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'dev.db',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'poc',
+        'USER': 'poc',
+        'PASSWORD': '$Pocdjango2016$',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
-URL_TESTE = 'DEV'
 
 
 
